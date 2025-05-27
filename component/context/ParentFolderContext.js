2c2088ecaa7740f15afd38196433a7335ebecee9
Keshav Goyal <161034870+Keshavgoyal14@users.noreply.github.com>
2025-05-27 17:00:51 +0530

    Update ParentFolderContext.js

--- component/context/ParentFolderContext.js
+++ component/context/ParentFolderContext.js
@@ -1,4 +1,9 @@
 'use client'
 import { createContext } from 'react';
 
-export const ParentFolderContext = createContext(null)
+export interface ParentFolderContextType {
+  parentFolderId: string | undefined;
+  setparentFolderId: (id: string | undefined) => void;
+}
+
+export const ParentFolderContext = createContext<ParentFolderContextType | null>(null);
