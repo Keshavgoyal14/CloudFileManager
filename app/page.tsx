0824c45b2b9d5d431d359771b82962719e155ebc
Keshav Goyal <161034870+Keshavgoyal14@users.noreply.github.com>
2025-05-27 14:38:10 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { useSession } from "next-auth/react";
 import Searchbar from "../component/Searchbar"
 import FolderList from "../component/FolderList"
 import FileList from "../component/fileList"
 import { useState } from "react";
 export default function Home() {
-  const { data: session,status} = useSession();
   const [search, setSearch] = useState("");
-const router = useRouter();
+
 
  return (
   <div>
